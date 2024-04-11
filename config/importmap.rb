d8798a581dff326b6093bebdc297432173032660
Daniel Mack <daniel@zonque.org>
2024-04-11 22:45:03 +0200

    Bump tempus-dominus and altcha

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,8 +8,8 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "popper", to: 'popper.js', preload: true
 
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js", preload: true
-pin "tempus-dominus", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.5/dist/js/tempus-dominus.min.js"
-pin "altcha", to: "https://cdn.jsdelivr.net/npm/altcha@0.2.2/+esm"
+pin "tempus-dominus", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.6/dist/js/tempus-dominus.min.js"
+pin "altcha", to: "https://cdn.jsdelivr.net/npm/altcha@0.2.3/+esm"
 pin "confetti", to: "https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/dist/confetti.browser.min.js"
 pin "maptilersdk", to: "https://cdn.jsdelivr.net/npm/@maptiler/sdk@1.2.1/dist/maptiler-sdk.umd.js"
 pin "maptilersdk-maptiler-geocoder", to: "https://cdn.maptiler.com/maptiler-geocoding-control/v1.2.0/maptilersdk.umd.js"
