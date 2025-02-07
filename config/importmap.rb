17b40ea2b4bee722e500776d3b676992340d0ff2
Daniel Mack <daniel@zonque.org>
2025-02-07 07:14:04 +0100

    update assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,10 +8,10 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "popper", to: 'popper.js', preload: true
 
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js", preload: true
-pin "tempus-dominus", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.6/dist/js/tempus-dominus.min.js"
-pin "tempus-dominus-bi-one", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.6/dist/plugins/bi-one.js"
-pin "altcha", to: "https://cdn.jsdelivr.net/npm/altcha@0.2.3/+esm"
-pin "confetti", to: "https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/dist/confetti.browser.min.js"
+pin "tempus-dominus", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.11/dist/js/tempus-dominus.min.js"
+pin "tempus-dominus-bi-one", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.11/dist/plugins/bi-one.js"
+pin "altcha", to: "https://cdn.jsdelivr.net/npm/altcha@1.1.1/+esm"
+pin "confetti", to: "https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"
 pin "maptilersdk", to: "https://cdn.jsdelivr.net/npm/@maptiler/sdk@1.2.1/dist/maptiler-sdk.umd.js"
 pin "maptilersdk-maptiler-geocoder", to: "https://cdn.maptiler.com/maptiler-geocoding-control/v1.2.0/maptilersdk.umd.js"
 pin "maptiler"
