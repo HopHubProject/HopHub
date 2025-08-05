f1a44ce944eb664af8b472ad3806f530372ff103
Daniel Mack <daniel@zonque.org>
2025-08-05 23:43:53 +0200

    Extend metrics output
    Report entries per event and status

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,20 +1,6 @@
 class MetricsController < ApplicationController
   def show
     @timestamp = (Time.now.to_f * 1000).to_i
-
-    @events = {
-      confirmed: Event.confirmed.count,
-      unconfirmed: Event.unconfirmed.count,
-    }
-
-    @entries = {
-      unconfirmed: Entry.unconfirmed.count,
-      offers_way_there: Entry.way_there.confirmed.count,
-      offers_way_back: Entry.way_back.confirmed.count,
-    }
-
-    @altcha_solutions = AltchaSolution.count
-
     render :show, layout: false, content_type: "text/plain, version=0.0.4"
   end
 end
