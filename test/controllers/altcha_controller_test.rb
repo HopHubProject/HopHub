ffecc9f97d2efab6e6b1e53f97836c53785b3f10
Daniel Mack <daniel.mack@holoplot.com>
2026-05-14 17:30:19 +0200

    Remove test for deleted controller

--- test/controllers/altcha_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class AltchaControllerTest < ActionDispatch::IntegrationTest
-  test "should receive altcha" do
-    get altcha_url
-    assert_response :success
-    assert_equal "application/json", @response.media_type
-  end
-end
