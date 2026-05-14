234eaa69b4cac0481b1b8138e72307689c79c175
Daniel Mack <daniel.mack@holoplot.com>
2026-05-14 17:26:05 +0200

    Bump more javascript assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,9 +7,9 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "popper", to: 'popper.js', preload: true
 
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js", preload: true
-pin "tempus-dominus", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.11/dist/js/tempus-dominus.min.js"
-pin "tempus-dominus-bi-one", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.9.11/dist/plugins/bi-one.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js", preload: true
+pin "tempus-dominus", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.10.4/dist/js/tempus-dominus.min.js"
+pin "tempus-dominus-bi-one", to: "https://cdn.jsdelivr.net/npm/@eonasdan/tempus-dominus@6.10.4/dist/plugins/bi-one.js"
 pin "altcha", to: "https://cdn.jsdelivr.net/npm/altcha@3.0.9/+esm"
-pin "confetti", to: "https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"
+pin "confetti", to: "https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.4/dist/confetti.browser.min.js"
 pin "geonames"
