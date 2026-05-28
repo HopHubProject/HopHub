26480dfc9ada056710da38b16cc464b73dead338
Daniel Mack <daniel@zonque.org>
2026-05-28 13:17:24 +0200

    Add French localization

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,7 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
 # Permitted locales available for the application
-I18n.available_locales = [:en, :de, :es]
+I18n.available_locales = [:en, :de, :es, :fr]
 
 # Set default locale to something other than :en
 I18n.default_locale = :en
